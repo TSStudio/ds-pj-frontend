a84cc5a51f93ee87eee334204e4a578ff4ec4497
Mingyang Tong <TSS@mail.tmysam.top>
2024-11-16 20:19:06 +0800

    Optimized route view

---
+++ src/route_helper.js
@@ -0,0 +1,99 @@
+export default {
+    /**
+     *
+     * @param {number} before direction in radian
+     * @param {number} after direction in radian
+     */
+    getTurning(before, after) {
+        let diff = after - before;
+        if (diff > Math.PI) {
+            diff -= 2 * Math.PI;
+        } else if (diff < -Math.PI) {
+            diff += 2 * Math.PI;
+        }
+        diff = (diff * 180) / Math.PI;
+        if (diff > 120) {
+            return "向左后方";
+        }
+        if (diff < -120) {
+            return "向右后方";
+        }
+        if (diff > 60) {
+            return "左转";
+        }
+        if (diff < -60) {
+            return "右转";
+        }
+        if (diff > 25) {
+            return "向左前方";
+        }
+        if (diff < -25) {
+            return "向右前方";
+        }
+        return "直行";
+    },
+    /**
+     * @param {Object} node1
+     * @param {Object} node2
+     * @returns {number} direction in radian
+     */
+    getDirection(node1, node2) {
+        let lat1 = node1.lat;
+        let lon1 = node1.lon;
+        let lat2 = node2.lat;
+        let lon2 = node2.lon;
+        let dLat = lat2 - lat1;
+        let dLon = lon2 - lon1;
+        let angle = Math.atan2(dLat, dLon);
+        return angle;
+    },
+    /**
+     *
+     * @param {Array} nodes
+     * @param {Array} paths
+     * @returns {Array}
+     */
+    getRouteDetails(nodes, paths) {
+        let result = [];
+        if (paths.length == 0) {
+            return result;
+        }
+        let lastRoadName = paths[0].name;
+        let totdistance = paths[0].distance;
+        let lastSegmentTurn = "出发";
+        let lastDirection = this.getDirection(
+            nodes[paths[0].start],
+            nodes[paths[0].end]
+        );
+        let lastMethod = paths[0].method;
+        for (let i = 1; i < paths.length; i++) {
+            let path = paths[i];
+            let node1 = nodes[path.start];
+            let node2 = nodes[path.end];
+            let direction = this.getDirection(node1, node2);
+            if (path.name != undefined) {
+                if (path.name != lastRoadName) {
+                    result.push({
+                        roadName: lastRoadName,
+                        distance: totdistance,
+                        direction: lastSegmentTurn,
+                        method: lastMethod,
+                    });
+                    lastSegmentTurn = this.getTurning(lastDirection, direction);
+                    lastRoadName = path.name;
+                    totdistance = path.distance;
+                }
+            }
+            totdistance += path.distance;
+            lastDirection = direction;
+            lastMethod = path.method;
+        }
+        result.push({
+            roadName: lastRoadName,
+            distance: totdistance,
+            direction: lastSegmentTurn,
+            method: lastMethod,
+        });
+        return result;
+    },
+};
---
+++ src/route_viewer.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-for="rt in route" class="route-view-row">
+        {{ getFriendlyRoute(rt) }}
+    </div>
+</template>
+<script>
+export default {
+    name: "RouteViewer",
+    methods: {
+        getFridndlyDistance(meter) {
+            if (meter < 999) {
+                return Math.round(meter) + "m";
+            } else {
+                return (meter / 1000).toFixed(2) + "km";
+            }
+        },
+        getFriendlyExecute(method) {
+            if (method == 1) {
+                return "步行";
+            }
+            if (method == 2) {
+                return "骑行";
+            }
+            if (method == 4) {
+                return "行驶";
+            }
+            if (method == 8 || method == 16) {
+                return "乘坐";
+            }
+        },
+        getFriendlyRoute(rt) {
+            return (
+                rt.direction +
+                "，进入" +
+                rt.roadName +
+                "，" +
+                this.getFriendlyExecute(rt.method) +
+                this.getFridndlyDistance(rt.distance)
+            );
+        },
+    },
+    props: {
+        route: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
